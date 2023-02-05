be66cb47d510ea56245800c49207e58478a9017b
Ankush263 <ankushbanik263@gmail.com>
2023-02-05 10:49:47 +0530

    update

---
+++ server/model/hospitalModel.js
@@ -0,0 +1,246 @@
+const mongoose = require("mongoose")
+
+
+const doctorSchema = new mongoose.Schema({
+  doctors: [
+    {
+      name: {
+        type: String,
+        required: [true, "must provide doctor name"]
+      },
+      walletAddress: {
+        type: String,
+        required: [true, "must provide doctor wallet address"]
+      },
+      image: {
+        type: String,
+        required: [true, "must provide doctor image"]
+      },
+      description: {
+        type: String,
+        required: [true, "must provide doctor description"]
+      },
+      gender: {
+        type: String,
+        required: [true, "must provide gender"]
+      },
+      specialistAt: {
+        type: String,
+        required: [true, "must provide specialist at"]
+      },
+      availableTime: {
+        type: String,
+        required: [true, "must provide available time"]
+      }
+    }
+  ]
+})
+
+const hospitalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must provide Hospital name"]
+  },
+  image: {
+    type: [String],
+    required: [true, "must provide Hospital image"]
+  },
+  location: {
+    type: String,
+    required: [true, "must provide Location"]
+  },
+  walletAddress: {
+    type: String,
+    required: [true, "must provide wallet address"]
+  },
+  description: {
+    type: String,
+    required: [true, "must provide Hospital description"]
+  },
+  telephone: {
+    type: [String],
+    required: [true, "must provide teliphone number"]
+  },
+  lat: {
+    type: String,
+    required: [true, "must provide lat"]
+  },
+  lng: {
+    type: String,
+    required: [true, "must provide lng"]
+  },
+  availableService: {
+    medicalProcidure: {
+      type: Boolean,
+      default: false
+    },
+    medicalTest: {
+      type: Boolean,
+      default: false
+    },
+    medicalTherapy: {
+      type: Boolean,
+      default: false
+    }
+  },
+  medicalSpecialty: {
+    Anesthesia: {
+      type: Boolean,
+      default: false
+    },
+    AnesthesiaDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Anesthesia
+      }
+    },
+    Dentistry: {
+      type: Boolean,
+      default: false
+    },
+    DentistryDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Dentistry
+      }
+    },
+    Dermatology: {
+      type: Boolean,
+      default: false
+    },
+    DermatologyDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Dermatology
+      }
+    },
+    DietNutrition: {
+      type: Boolean,
+      default: false
+    },
+    DietNutritionDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.DietNutrition
+      }
+    },
+    Emergency: {
+      type: Boolean,
+      default: false
+    },
+    EmergencyDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Emergency
+      }
+    },
+    Gynecologic: {
+      type: Boolean,
+      default: false
+    },
+    GynecologicDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Gynecologic
+      }
+    },
+    Neurologic: {
+      type: Boolean,
+      default: false
+    },
+    NeurologicDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Neurologic
+      }
+    },
+    Pathology: {
+      type: Boolean,
+      default: false
+    },
+    PathologyDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Pathology
+      }
+    },
+    Pediatric: {
+      type: Boolean,
+      default: false
+    },
+    PediatricDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Pediatric
+      }
+    },
+    PlasticSurgery: {
+      type: Boolean,
+      default: false
+    },
+    PlasticSurgeryDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.PlasticSurgery
+      }
+    },
+    Psychiatric: {
+      type: Boolean,
+      default: false
+    },
+    PsychiatricDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Psychiatric
+      }
+    },
+    Renal: {
+      type: Boolean,
+      default: false
+    },
+    RenalDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Renal
+      }
+    },
+    Surgical: {
+      type: Boolean,
+      default: false
+    },
+    SurgicalDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Surgical
+      }
+    },
+    Toxicologic: {
+      type: Boolean,
+      default: false
+    },
+    ToxicologicDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Toxicologic
+      }
+    },
+    Urologic: {
+      type: Boolean,
+      default: false
+    },
+    UrologicDoctor: {
+      type: doctorSchema,
+      validate: function() {
+        return this.Urologic
+      }
+    },
+  },
+  openingHours: {
+    type: String,
+    required: [true, "must provide opening hours"]
+  }
+})
+
+const Hospital = mongoose.model("Hospital", hospitalSchema)
+
+module.exports = Hospital
