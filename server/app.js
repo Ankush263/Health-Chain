886cbe162c9d1bd745ed3782606fd76355535424
Ankush263 <ankushbanik263@gmail.com>
2023-02-05 10:49:17 +0530

    update

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const hospitalRouter = require("./routes/hospitalRoutes.js")
+const AppError = require("./utils/appError")
+
+app.use(cors())
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log("This is from middleware")
+  next()
+})
+
+app.use("/api/v1/hospital", hospitalRouter)
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404))
+})
+
+module.exports = app
