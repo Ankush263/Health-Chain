bbeba7f4ce56724431b19cf9abbbb9691f64418c
Ankush263 <ankushbanik263@gmail.com>
2023-02-06 10:27:06 +0530

    update

---
+++ server/utils/apiFeatures.js
@@ -0,0 +1,44 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query
+    this.queryString = queryString
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString }
+    const excludeFields = ["page", "sort", "limit", "fields"]
+    excludeFields.forEach(el => delete queryObj[el])
+
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+    this.query = this.query.find(JSON.parse(queryStr))
+    return this
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+
+  sort() {
+    if(this.queryString.sort) {
+      this.query = this.query.sort(this.queryString.sort)
+    }
+    return this
+  }
+
+  pagination() {
+    const page = this.queryString.page * 1 || 1
+    const limit = this.queryString.limit * 1 || 10
+    const skip = (page-1) * limit
+    this.query = this.query.skip(skip).limit(limit)
+    return this
+  }
+}
+
+module.exports = ApiFeatures
