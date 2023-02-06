f2e65d48f5a8def9e43dc2a96190b345c554ce34
Ankush263 <ankushbanik263@gmail.com>
2023-02-06 10:26:40 +0530

    update

--- server/model/hospitalModel.js
+++ server/model/hospitalModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
 
 
 const doctorSchema = new mongoose.Schema({
@@ -11,6 +12,13 @@ const doctorSchema = new mongoose.Schema({
       walletAddress: {
         type: String,
         required: [true, "must provide doctor wallet address"]
+      },
+      email: {
+        type: String,
+        required: [true, "please provide doctor email"],
+        unique: true,
+        lowercase: true,
+        validator: [validator.isEmail, "Please provide valid email"]
       },
       image: {
         type: String,
@@ -40,6 +48,13 @@ const hospitalSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Must provide Hospital name"]
+  },
+  email: {
+    type: String,
+    required: [true, "please provide hospital email"],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, "Please provide valid email"]
   },
   image: {
     type: [String],
@@ -83,157 +98,155 @@ const hospitalSchema = new mongoose.Schema({
       default: false
     }
   },
-  medicalSpecialty: {
-    Anesthesia: {
-      type: Boolean,
-      default: false
-    },
-    AnesthesiaDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Anesthesia
-      }
-    },
-    Dentistry: {
-      type: Boolean,
-      default: false
-    },
-    DentistryDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Dentistry
-      }
-    },
-    Dermatology: {
-      type: Boolean,
-      default: false
-    },
-    DermatologyDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Dermatology
-      }
-    },
-    DietNutrition: {
-      type: Boolean,
-      default: false
-    },
-    DietNutritionDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.DietNutrition
-      }
-    },
-    Emergency: {
-      type: Boolean,
-      default: false
-    },
-    EmergencyDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Emergency
-      }
-    },
-    Gynecologic: {
-      type: Boolean,
-      default: false
-    },
-    GynecologicDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Gynecologic
-      }
-    },
-    Neurologic: {
-      type: Boolean,
-      default: false
-    },
-    NeurologicDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Neurologic
-      }
-    },
-    Pathology: {
-      type: Boolean,
-      default: false
-    },
-    PathologyDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Pathology
-      }
-    },
-    Pediatric: {
-      type: Boolean,
-      default: false
-    },
-    PediatricDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Pediatric
-      }
-    },
-    PlasticSurgery: {
-      type: Boolean,
-      default: false
-    },
-    PlasticSurgeryDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.PlasticSurgery
-      }
-    },
-    Psychiatric: {
-      type: Boolean,
-      default: false
-    },
-    PsychiatricDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Psychiatric
-      }
-    },
-    Renal: {
-      type: Boolean,
-      default: false
-    },
-    RenalDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Renal
-      }
-    },
-    Surgical: {
-      type: Boolean,
-      default: false
-    },
-    SurgicalDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Surgical
-      }
-    },
-    Toxicologic: {
-      type: Boolean,
-      default: false
-    },
-    ToxicologicDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Toxicologic
-      }
-    },
-    Urologic: {
-      type: Boolean,
-      default: false
-    },
-    UrologicDoctor: {
-      type: doctorSchema,
-      validate: function() {
-        return this.Urologic
-      }
-    },
+  Anesthesia: {
+    type: Boolean,
+    default: false
+  },
+  AnesthesiaDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Anesthesia
+    }
+  },
+  Dentistry: {
+    type: Boolean,
+    default: false
+  },
+  DentistryDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Dentistry
+    }
+  },
+  Dermatology: {
+    type: Boolean,
+    default: false
+  },
+  DermatologyDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Dermatology
+    }
+  },
+  DietNutrition: {
+    type: Boolean,
+    default: false
+  },
+  DietNutritionDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.DietNutrition
+    }
+  },
+  Emergency: {
+    type: Boolean,
+    default: false
+  },
+  EmergencyDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Emergency
+    }
+  },
+  Gynecologic: {
+    type: Boolean,
+    default: false
+  },
+  GynecologicDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Gynecologic
+    }
+  },
+  Neurologic: {
+    type: Boolean,
+    default: false
+  },
+  NeurologicDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Neurologic
+    }
+  },
+  Pathology: {
+    type: Boolean,
+    default: false
+  },
+  PathologyDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Pathology
+    }
+  },
+  Pediatric: {
+    type: Boolean,
+    default: false
+  },
+  PediatricDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Pediatric
+    }
+  },
+  PlasticSurgery: {
+    type: Boolean,
+    default: false
+  },
+  PlasticSurgeryDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.PlasticSurgery
+    }
+  },
+  Psychiatric: {
+    type: Boolean,
+    default: false
+  },
+  PsychiatricDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Psychiatric
+    }
+  },
+  Renal: {
+    type: Boolean,
+    default: false
+  },
+  RenalDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Renal
+    }
+  },
+  Surgical: {
+    type: Boolean,
+    default: false
+  },
+  SurgicalDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Surgical
+    }
+  },
+  Toxicologic: {
+    type: Boolean,
+    default: false
+  },
+  ToxicologicDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Toxicologic
+    }
+  },
+  Urologic: {
+    type: Boolean,
+    default: false
+  },
+  UrologicDoctor: {
+    type: doctorSchema,
+    validate: function() {
+      return this.Urologic
+    }
   },
   openingHours: {
     type: String,
