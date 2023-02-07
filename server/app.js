e0c7a478b4dd825b7a431c83d43a893b6d71e2c2
Ankush263 <ankushbanik263@gmail.com>
2023-02-07 21:28:54 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+
 const hospitalRouter = require("./routes/hospitalRoutes.js")
 const patientRouter = require("./routes/patientRoutes.js")
+const bookingRouter = require("./routes/bookingRoutes.js")
+
 const AppError = require("./utils/appError")
+const globalErrorHandler = require("./controllers/errorControllers.js")
 
 app.use(cors())
 app.use(express.json())
@@ -15,9 +19,12 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/hospital", hospitalRouter)
 app.use("/api/v1/patient", patientRouter)
+app.use("/api/v1/booking", bookingRouter)
 
 app.all("*", (req, res, next) => {
   next(new AppError(`can't find ${req.originalUrl} on this server`, 404))
 })
+
+app.use(globalErrorHandler)
 
 module.exports = app
