1ff5ab32768acb042d41f60e20eb82ad4371f11d
Ankush263 <ankushbanik263@gmail.com>
2023-02-07 21:29:34 +0530

    update

--- server/model/patientModel.js
+++ server/model/patientModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
+const bcrypt = require("bcryptjs")
 
 const patientSchema = new mongoose.Schema({
   name: {
@@ -8,14 +9,8 @@ const patientSchema = new mongoose.Schema({
   },
   walletAddress: {
     type: String,
-    required: [true, "must provide patient wallet address"]
-  },
-  email: {
-    type: String,
-    required: [true, "please provide patient email"],
     unique: true,
-    lowercase: true,
-    validator: [validator.isEmail, "Please provide valid email"]
+    required: [true, "must provide patient wallet address"]
   },
   age: {
     type: Number,
@@ -25,13 +20,53 @@ const patientSchema = new mongoose.Schema({
     type: String,
     required: [true, "must provide patient gender"]
   },
-  height: {
-    type: String
+  email: {
+    type: String,
+    required: [true, "please provide patient email"],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, "Please provide valid email"]
   },
-  weight: {
-    type: String
-  }
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+    select: false
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el == this.password
+      },
+      message: "Password is not same"
+    }
+  },
+  passwordChangedAt: Date
 })
+
+patientSchema.pre("save", async function(next) {
+  if(!this.isModified("password")) return next()
+
+  this.password = await bcrypt.hash(this.password, 12)
+  this.passwordConfirm = undefined
+  next()
+})
+
+patientSchema.methods.correctPassword = async function(
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
+
+patientSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if(this.passwordChangedAt) {
+    console.log(this.passwordChangedAt, JWTTimestamp)
+  }
+  return false
+}
 
 const Patient = mongoose.model("Patient", patientSchema)
 
