cd7602c4c79875d651c184b4fb976686200ebb71
Ankush263 <ankushbanik263@gmail.com>
2023-02-07 21:29:21 +0530

    update

--- server/controllers/PatientControllers.js
+++ server/controllers/PatientControllers.js
@@ -3,17 +3,6 @@ const catchAsync = require("../utils/catchAsync.js")
 const AppError = require("../utils/appError.js")
 const ApiFeatures = require("../utils/apiFeatures.js")
 
-// ----------CREATE PATIENT----------
-exports.createPatient = catchAsync(async (req, res, next) => {
-  const newPatient = await Patient.create(req.body)
-  res.status(201).json({
-    status: "Sucess",
-    data: {
-      patient: newPatient
-    }
-  })
-})
-
 // ----------GET ALL PATIENT----------
 exports.getPatient = catchAsync(async (req, res, next) => {
   const features = new ApiFeatures(Patient.find(), req.query)
@@ -50,31 +39,3 @@ exports.getSinglePatient = catchAsync(async (req, res, next) => {
   })
 })
 
-// ----------UPDATE PATIENT----------
-exports.updatePatient = catchAsync(async (req, res, next) => {
-  const patient = await Patient.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidator: true
-  })
-  if(!patient) {
-    return next(new AppError("No patient found with that ID"), 404)
-  }
-  res.status(200).json({
-    status: "Success",
-    data: {
-      patient
-    }
-  })
-})
-
-// ----------DELETE PATIENT----------
-exports.deletePatient = catchAsync(async (req, res, next) => {
-  const patient = await Patient.findByIdAndDelete(req.params.id)
-  if(!patient) {
-    return next(new AppError("No patient found with that ID"), 404)
-  }
-  res.status(204).json({
-    status: "Success",
-    data: null
-  })
-})
