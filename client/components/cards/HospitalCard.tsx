0d58a4ed36bafc1d502aed8299ec9e58b5022abc
Ankush263 <ankushbanik263@gmail.com>
2023-02-10 10:03:32 +0530

    update

--- client/components/cards/HospitalCard.tsx
+++ client/components/cards/HospitalCard.tsx
@@ -4,8 +4,8 @@ function HospitalCard() {
 
   const styles = {
     main_container: `w-96 h-96 flex flex-col justify-between items-center`,
-    img_container: `w-8/12 h-4/6 drop-shadow-2.5xl`,
-    name_container: `border-2 mb-10 h-10 min-w-6/12 bg-white drop-shadow-2xl p-2 contain`
+    img_container: `w-8/12 h-4/6 drop-shadow-2.5xl active:mt-2 active:drop-shadow-2xl`,
+    name_container: `border-2 mb-10 h-10 min-w-6/12 bg-white drop-shadow-2xl p-2 contain active:mt-2 active:drop-shadow-xl`
   }
 
   return (
