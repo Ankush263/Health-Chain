e2101b67465a1aa7b156f2f019b8ca068561bf20
Ankush263 <ankushbanik263@gmail.com>
2023-02-10 10:03:07 +0530

    update

---
+++ client/components/cards/BookingPopup.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function BookingPopup() {
+
+  const styles = {
+    main: `flex flex-col justify-center items-center h-full w-full`,
+    input: `w-8/12 border-2 bg-white drop-shadow-2xl h-16 mb-5`
+  }
+
+  return (
+    <div className={styles.main}>
+      <input type="date" className={styles.input} name="" id="" />
+      <input type="time" className={styles.input} name="" id="" />
+      <button type="submit" className='bg-dark_pink w-28 h-10 border-2 drop-shadow-2xl active:mt-2 active:drop-shadow-xl'>Book Now</button>
+    </div>
+  )
+}
+
+export default BookingPopup
