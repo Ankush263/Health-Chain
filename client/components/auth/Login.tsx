bf6c95b7df212320ce1e0be0792d2e99552edf54
Ankush263 <ankushbanik263@gmail.com>
2023-02-11 11:41:17 +0530

    update

--- client/components/auth/Login.tsx
+++ client/components/auth/Login.tsx
@@ -1,6 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { LoginContext } from '../../Helper/Context'
+import { ethers } from 'ethers'
+import { login } from '../../Api'
+import Swal from 'sweetalert2'
 
 function Login() {
+  const [connected, setConnected] = useState(false)
+  const context = useContext(LoginContext)
+  const [loginForm, setLoginForm] = useState({
+    walletAddress: '',
+    email: '',
+    password: ''
+  })
+
+  let provider: any
+  let signer: any
+  if(typeof window !== 'undefined') {
+    provider = new ethers.providers.Web3Provider(window.ethereum)
+    signer = provider.getSigner()
+  }
+
+  const handleLogin = async () => {
+    try {
+      const response = await login({
+        email: loginForm.email,
+        password: loginForm.password
+      })
+
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Congratulation, You successfully Login...',
+        showConfirmButton: false,
+        timer: 1500
+      })
+
+      // context.setLogin(true)
+      const token = response.data.token
+      let expiration = new Date().getTime() + 60 * 60 * 24 * 30 * 1000  // 30 days
+      localStorage.setItem("Token", JSON.stringify({ value: `${token}`, expires: expiration }))
+      // console.log(token)
+      window.location.reload()
+
+    } catch (error: any) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error.response.data.message}`,
+      })
+      console.log(error)
+    }
+  }
+
+  const handleConnect = async () => {
+    try{
+      
+      if (typeof window !== 'undefined') {
+        // const chainId = await window.ethereum.request({ method: 'eth_chainId' })
+        // if(chainId != '0x13881') {
+        //   await window.ethereum.request({
+        //     method: 'wallet_switchEthereumChain',
+        //     params: [{ chainId: '0x13881' }],
+        //   })
+        // }
+        await window.ethereum.request({ method: 'eth_requestAccounts' })
+        setConnected(true)
+        setLoginForm({...loginForm, walletAddress: await signer.getAddress()})
+      }
+    }catch(err) {
+      console.log(err)
+    }
+  }
 
   const styles = {
     main_box: `bg-dark_pink w-full drop-shadow-3xl border-4 flex flex-col justify-around p-10`,
@@ -14,8 +84,8 @@ function Login() {
   return (
     <div className={styles.main_box}>
       <div className={styles.input_bg}>
-        <button className={styles.connect_wallet}>
-          <span>Connect Wallet</span>
+        <button className={styles.connect_wallet} onClick={handleConnect}>
+          <span>{connected ? "Connected" : "Connect Wallet"}</span>
         </button>
       </div>
       <div className={styles.input_bg}>
@@ -24,6 +94,7 @@ function Login() {
             type="email"
             className='w-full h-full p-2 placeholder:text-2xl'
             placeholder='email:'
+            onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
           />
         </div>
       </div>
@@ -34,14 +105,22 @@ function Login() {
             type="password"
             className='w-full h-full p-2 placeholder:text-2xl'
             placeholder='password:'
+            onChange={(e) => setLoginForm({ ...loginForm, password: e.target.value })}
           />
         </div>
       </div>
 
       <div className={styles.input_bg}>
-        <button className={styles.signup}>
+        <button className={styles.signup} onClick={handleLogin}>
           <span>login</span>
         </button>
+      </div>
+      <div className='w-full flex justify-center items-center'>
+        <span onClick={() => {
+          window.location.reload()
+        }} className='text-sm cursor-pointer border-b-2'>
+          Don't have an account, Signup...
+        </span>
       </div>
     </div>
   )
