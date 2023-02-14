e52e5d4bad5a8cb2673fa9df5c8161bdec018550
Ankush263 <ankushbanik263@gmail.com>
2023-02-14 10:57:02 +0530

    update

--- client/components/cards/BookingPopup.tsx
+++ client/components/cards/BookingPopup.tsx
@@ -4,14 +4,15 @@ function BookingPopup() {
 
   const styles = {
     main: `flex flex-col justify-center items-center h-full w-full`,
-    input: `w-8/12 border-2 bg-white drop-shadow-2xl h-16 mb-5`
+    input: `w-8/12 border-2 bg-white drop-shadow-2xl h-16 mb-5`,
+    btn: `bg-dark_pink w-28 h-10 border-2 drop-shadow-2xl active:mt-2 active:drop-shadow-xl`
   }
 
   return (
     <div className={styles.main}>
       <input type="date" className={styles.input} name="" id="" />
       <input type="time" className={styles.input} name="" id="" />
-      <button type="submit" className='bg-dark_pink w-28 h-10 border-2 drop-shadow-2xl active:mt-2 active:drop-shadow-xl'>Book Now</button>
+      <button type="submit" className={styles.btn}>Book Now</button>
     </div>
   )
 }
