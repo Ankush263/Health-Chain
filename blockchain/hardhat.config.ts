9848e0ac35b0399e0f82b83ec701865b4a2c120b
Ankush263 <ankushbanik263@gmail.com>
2023-02-17 16:04:14 +0530

    update

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,30 @@
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.17",
+// };
+
+// export default config;
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
+
+const PRIVATE_KEY: any = process.env.PRIVATE_KEY;
+// const RPC_URL: any = process.env.RPC_URL
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
